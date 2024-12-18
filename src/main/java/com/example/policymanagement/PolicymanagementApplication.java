361de6ae61dcf550486b81db6e717953714a766b
abinaspathy <abinaspathy@gmnail.com>
2024-12-18 15:10:56 +0530

    main class changed

--- src/main/java/com/example/policymanagement/PolicymanagementApplication.java
+++ src/main/java/com/example/policymanagement/PolicymanagementApplication.java
@@ -2,9 +2,14 @@ package com.example.policymanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.ui.ModelMap;
 
 @SpringBootApplication
 public class PolicymanagementApplication {
+	public ModelMap modelMap(){
+		return new ModelMap();
+
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PolicymanagementApplication.class, args);
